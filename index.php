53ad5d451a229674341863878fdbe7d357139517
PKrystian <krystian.pinczak2001@gmail.com>
2023-05-19 23:41:28 +0200

    Added: 3 functions: DB: Connection UserData: Import, Print Edited index to .php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank App!</title>
+</head>
+<body>
+        <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Password</th>
+            <th>Address</th>
+            <th>PESEL</th>
+            <th>Email</th>
+            <th>Balance</th>
+            <th>Phone number</th>
+            <th>Date opened</th>
+            <th>Role ID</th>
+            <th>Account number</th>
+        </tr>
+
+        <?php include "scripts/print_user.php"; ?>
+        
+    </table>
+</body>
+</html>
