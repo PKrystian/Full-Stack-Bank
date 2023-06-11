39359dd57f52ab0cdbda973ad0e8d5e748c6eab2
PKrystian <krystian.pinczak2001@gmail.com>
2023-06-11 13:01:46 +0200

    Added history, transaction option for user and consultant page

--- pages/panels/history_panel_user.php
+++ pages/panels/history_panel_user.php
@@ -2,22 +2,20 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <title>Main Panel</title>
+    <title>Transaction History</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="../../style/styles.css" rel="stylesheet" type="text/css" />
   </head>
   <body class="body">
-
     <?php
       session_start();
-         
-      if (isset($_SESSION["success_message"]))
-        {
-            echo "<script>alert('" . $_SESSION['success_message'] . "');</script>";
-            unset($_SESSION["success_message"]);
-        }
-    ?>
 
+      if (isset($_SESSION["success_message"]))
+      {
+        echo "<script>alert('" . $_SESSION['success_message'] . "');</script>";
+        unset($_SESSION["success_message"]);
+      }
+    ?>
     <div class="navbar-logo-left">
       <div class="container">
         <div class="navbar-wrapper">
@@ -70,39 +68,8 @@
       </div>
     </div>
     <section class="section">
-      <h1 class="heading">Accounts</h1>
-      <div class="div-block-accounts">
-        <div>
-          <h5 class="account-info-1">
-            <?php echo isset($_SESSION["first_name"]) && isset($_SESSION["last_name"]) ? $_SESSION["first_name"] . " " . $_SESSION["last_name"] : ""; ?>
-          </h5>
-          <h6 class="account-info-2">
-            <?php echo isset($_SESSION["account_number"]) ? $_SESSION["account_number"] : ""; ?>
-          </h6>
-        </div>
-        <div>
-          <h5 class="account-info-1">
-            <!-- todo: fix issue where after transfer, view of balance doesn"t update (it updates in db, you can relog to check it) -->
-            <?php echo isset($_SESSION["balance"]) ? $_SESSION["balance"] . " PLN" : ""; ?>
-          </h5>
-          <h6 class="account-info-2">available funds</h6>
-        </div>
-      </div>
-    </section>
-    <section class="section">
-      <h1 class="heading">Your cards</h1>
-      <div class="div-block-cards">
-        <img src="../img/blue-card.svg" width="200" />
-        <div class="div-block-card-info">
-          <div class="card-name">VISA</div>
-          <div class="card-number">
-            <?php echo isset($_SESSION["account_number"]) ? $_SESSION["account_number"] : ""; ?>
-          </div>
-          <div class="card-owner">
-            <?php echo isset($_SESSION["first_name"]) && isset($_SESSION["last_name"]) ? $_SESSION["first_name"] . " " . $_SESSION["last_name"] : ""; ?>
-          </div>
-        </div>
-      </div>
+      <h1 class="heading">Transaction History</h1>
+      <?php include "../../scripts/history.php"; ?>
     </section>
     <script src="./scipts.js" type="text/javascript"></script>
   </body>
